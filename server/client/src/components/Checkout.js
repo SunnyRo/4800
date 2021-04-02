18776eac8e03791f299b7a50916acb74ec5a101b
DavidTran <dtrancpp@gmail.com>
2021-04-02 00:26:42 -0700

    Checkout page and css

--- server/client/src/components/Checkout.js
+++ server/client/src/components/Checkout.js
@@ -1,7 +1,12 @@
 import { React, Component } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
-import { Button, createMuiTheme, ThemeProvider } from "@material-ui/core";
+import {
+    Button,
+    createMuiTheme,
+    Divider,
+    ThemeProvider,
+} from "@material-ui/core";
 import "./css/Checkout.css";
 
 const theme = createMuiTheme({
@@ -51,55 +56,78 @@ class Checkout extends Component {
             <div className="checkout">
                 <ThemeProvider theme={theme}>
                     <Header />
-                    {cartInfo.map((product) => (
-                        <div className="product_layout">
-                            <div className="store_name">{product.store}</div>
-                            <div className="product_image_container">
-                                <img
-                                    className="product_image"
-                                    src={product.photo}
-                                ></img>
-                            </div>
-                            <div className="product_details">
-                                <div className="product_name">
-                                    {product.name}
+                    <div className="flex_container">
+                        <div className="products_container">
+                            {cartInfo.map((product) => (
+                                <div className="product_layout">
+                                    <div className="product_details_flexbox1">
+                                        <img
+                                            className="product_image"
+                                            src={product.photo}
+                                        ></img>
+                                        <div className="product_details_name_type">
+                                            <div className="product_name">
+                                                {product.name}
+                                            </div>
+                                            <div className="product_type">
+                                                Type: {product.type}
+                                            </div>
+                                        </div>
+                                        <div className="product_details_quantity">
+                                            <div className="product_quantity">
+                                                Quantity: {cart[product.id]}
+                                            </div>
+                                        </div>
+                                        <div className="product_details_price">
+                                            <div className="product_price">
+                                                $
+                                                {parseFloat(
+                                                    product.price
+                                                ).toFixed(2)}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="store_and_delivery">
+                                        <div className="store_details">
+                                            <div className="store_name">
+                                                {product.store}
+                                            </div>
+                                            <div className="store_address">
+                                                {product.address}
+                                            </div>
+                                            <div className="store_phone">
+                                                {product.phone}
+                                            </div>
+                                            <div className="store_distance">
+                                                {convertDistance(
+                                                    storeDistances[
+                                                        product.store
+                                                    ]
+                                                )}
+                                                {" miles away"}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <Button
+                                        className="remove_button"
+                                        color="primary"
+                                        productID={product.id}
+                                        onClick={this.removeFromCart}
+                                    >
+                                        Remove
+                                    </Button>
                                 </div>
-                                <div className="product_price">
-                                    ${parseFloat(product.price).toFixed(2)}
-                                </div>
-                                <div className="product_type">
-                                    Type: {product.type}
-                                </div>
-                                <div className="product_quantity">
-                                    Quantity: {cart[product.id]}
-                                </div>
-                            </div>
-                            <div className="store_details">
-                                <div className="store_address">
-                                    {product.address}
-                                </div>
-                                <div className="store_phone">
-                                    {product.phone}
-                                </div>
-                                <div className="store_distance">
-                                    {convertDistance(
-                                        storeDistances[product.store]
-                                    )}
-                                    {" miles away"}
-                                </div>
-                            </div>
-                            <Button
-                                className="remove_button"
-                                color="primary"
-                                productID={product.id}
-                                onClick={this.removeFromCart}
-                            >
-                                Remove
-                            </Button>
+                            ))}
                         </div>
-                    ))}
-                    <div className="checkout_details">
-                        Total Price: blah blah
+                        <div className="checkout_details">
+                            <div className="order_summary">Order Summary:</div>
+                            <div className="subtotal">{"Subtotal: "}</div>
+                            <div className="delivery_fees">
+                                {"Delivery fees: "}
+                            </div>
+                            <div className="tax_fee">{"Tax fees: "}</div>
+                            <div className="order_total">{"Order total: "}</div>
+                        </div>
                     </div>
                     <Button
                         className="return_to_cart"
